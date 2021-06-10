0fc0a477c2e58c21424d56e91741a9af5d6d8920
jefferyl <clay615040@hotmail.com>
2021-06-10 10:32:37 +0800

    切版-主頁

--- src/App.vue
+++ src/App.vue
@@ -29,15 +29,42 @@
         .trello__header__kanban__icon
         .trello__header__kanban__text 看板
         .trello__header__kanban__arrayIcon
-      .trello__header__kanbanName test
+      .trello__header__kanbanName 看板名稱
       .trello__header__star
-      .trello__header__user 訪客
-      .trello__header__work 開放觀看權限的工作區
+        .trello__header__star__icon
+      .trello__header__line
+      .trello__header__user
+        .trello__header__user__text 訪客
+        .trello__header__user__type Free
+      .trello__header__line
+      .trello__header__work
+        .trello__header__work__icon
+        .trello__header__work__text 開放觀看權限的工作區
+      .trello__header__line
       .trello__header__userGroup 訪客
       .trello__header__invite 邀請
     .trello__header__right
-      .trello__header__invite Butler
-      .trello__header__menu 顯示選單
+      .trello__header__invite
+        .trello__header__invite__icon
+        .trello__header__invite__text Butler
+      .trello__header__menu
+        .trello__header__menu__icon
+        .trello__header__menu__text 顯示選單
+  .trello__main
+    .trello__main__item
+      .trello__main__item__title
+        .trello__main__item__title__text 列表名稱
+        .trello__main__item__title__btn
+          .trello__main__item__title__icon
+      .trello__main__item__main
+        .trello__main__item__addCard
+          .trello__main__item__addCard__icon
+          .trello__main__item__addCard__text 新增卡片
+        .trello__main__item__templateCard
+          .trello__main__item__templateCard__icon
+    .trello__main__add
+      .trello__main__add__icon
+      .trello__main__add__text 新增一個列表
 </template>
 
 <script>
